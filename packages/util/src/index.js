3aebd2829dc59567824b6bba39b120688ec0019b
Guylian Cox <gc@madkings.com>
2018-11-13 15:59:38 +0100

    feat(util): provide coalesce & asyncGlob

--- packages/util/src/index.js
+++ packages/util/src/index.js
@@ -1,5 +1,9 @@
 // @flow
 
+import assert from 'assert';
+import pathUtil from 'path';
+import glob from 'glob';
+
 export function hasOwnProperty(obj: Object, propertyKey: string): boolean {
   return Object.prototype.hasOwnProperty.call(obj, propertyKey);
 }
@@ -8,4 +12,38 @@ export function isPlainObject(obj: Object) {
   const proto = Object.getPrototypeOf(obj);
 
   return proto == null || proto === Object.prototype;
+}
+
+export function asyncGlob(path: string, options: Object = {}): Promise<string[]> {
+  return new Promise((resolve, reject) => {
+
+    const newOptions = {
+      cwd: asyncGlob.cwd,
+      ...options,
+    };
+
+    glob(path, newOptions, (err, files) => {
+      if (err) {
+        return void reject(err);
+      }
+
+      // make files paths absolute
+      files = files.map(file => pathUtil.join(asyncGlob.cwd || '', file));
+
+      resolve(files);
+    });
+  });
+}
+
+export function coalesce(...args) {
+  assert(args.length > 0, 'Must have at least one argument');
+
+  for (let i = 0; i < args.length - 1; i++) {
+    const arg = args[i];
+    if (arg != null) {
+      return arg;
+    }
+  }
+
+  return args[args.length - 1];
 }
