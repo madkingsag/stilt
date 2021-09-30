27eace8b72ada40d8d8da6f6f8631a7c194e26fd
ephys <hello@guylian.me>
2021-09-30 11:06:57 +0200

    chore: update dependencies & remove flow

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// eslint-disable-next-line import/no-commonjs
 module.exports = {
   presets: [
     ['@babel/preset-env', {
@@ -8,32 +7,11 @@ module.exports = {
         node: '12.18.3',
       },
     }],
+    '@babel/preset-typescript',
   ],
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     '@babel/plugin-syntax-class-properties',
   ],
   sourceMaps: 'inline',
-  overrides: [{
-    test: ['**/*.js'],
-    presets: [
-      ['@babel/preset-env', {
-        targets: {
-          node: '12.18.3',
-        },
-      }],
-      '@babel/preset-flow',
-    ],
-
-  }, {
-    test: ['**/*.ts'],
-    presets: [
-      ['@babel/preset-env', {
-        targets: {
-          node: '12.18.3',
-        },
-      }],
-      '@babel/preset-typescript',
-    ],
-  }],
 };
